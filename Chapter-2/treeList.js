2b4171586945db33d629e76b1975ac0bd51134e9
Show Wai Yan <showwaiyan555@gmail.com>
2025-01-21 07:48:05 +0800

    Tree operations constructed by pair data structure finished

---
+++ Chapter-2/treeList.js
@@ -0,0 +1,46 @@
+function pair(x,y) {
+    return {
+        head: x,
+        tail: y
+    }
+}
+
+function head(p) {
+    return p.head;
+}
+
+function tail(p) {
+   return p.tail;
+}
+
+function isNull(items) {
+    return items === null;
+}
+
+function isPair(x) {
+    return hasHead(x) && hasTail(x);
+}
+
+function hasHead(x) {
+    return Object.hasOwn(x,'head');
+}
+
+function hasTail(x) {
+    return Object.hasOwn(x,'tail')
+}
+
+function countLeaves(x) {
+    return isNull(x) ?
+            0 : !isPair(x) ?
+                1 : countLeaves(head(x)) + countLeaves(tail(x));
+}
+
+function map(fun,x) {
+    return isNull(x) ?
+        null : !isPair(x) ?
+            fun(x) : pair(map(fun,head(x)),map(fun,tail(x)));
+}
+
+const x = pair(pair(1,2),pair(3,4))
+console.log(countLeaves(x));
+console.log(map(x=>x*2,x));
